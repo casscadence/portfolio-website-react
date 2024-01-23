8bafbfc27ec64f63a42afa3fae8303084e47204d
casscadence <casscadence@gmail.com>
2024-01-22 22:42:42 -0500

    changing names

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './ap'
+import App from './app'
 import './index.scss'
 import { BrowserRouter } from 'react-router-dom'
 
