45bc495ef9cadb01df9db0bba256becd2f026d7e
casscadence <casscadence@gmail.com>
2024-01-23 01:07:44 -0500

    function removed

--- client/src/components/projectimages/index.js
+++ client/src/components/projectimages/index.js
@@ -4,7 +4,7 @@ const ProjectImages = ({ imgFolder, imgArray, subIndex, addIndex, direction }) =
     <>
         {imgArray.map((char, i) => (
             <div key={char} className={`rotating-image ${direction}`} onClick={char+1===2 ? addIndex : char+1===4 ? subIndex : ''} data-arrival-index={char+1}>
-              <img src={require(`../../assets/images/${imgFolder}/1-${i.toLowerCase()}.png`)} alt='' />  
+              <img src={require(`../../assets/images/${imgFolder}/1-${i}.png`)} alt='' />  
             </div>
         ))}
     </>
