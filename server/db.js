35ad89c42f1664a4c609cd6eae8e3f2585606408
casscadence <casscadence@gmail.com>
2024-01-23 01:00:21 -0500

    railway updated

--- server/db.js
+++ server/db.js
@@ -1,12 +1,11 @@
+require("dotenv").config();
 const { createPool } = require('mysql');
+
+const urlDB = 'mysql://root:E-3A5333c6GghGAFhhh3g3d2c434Ab6C@viaduct.proxy.rlwy.net:35738/railway';
 
 const getConnection = () => {
     const pool = createPool({
-        host: 'localhost',
-        user: 'root',
-        password: 'StarryLimit44!',
-        database: 'portfolio_projects',
-        connectionLimit: 10
+        urlDB
     })
 }
 
