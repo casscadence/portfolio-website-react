165f797359f26c61324b329f72bcdcb3bd19ae87
casscadence <casscadence@gmail.com>
2024-03-02 04:59:04 -0500

    be testing

--- server/server.js
+++ server/server.js
@@ -71,7 +71,7 @@ const port = process.env.PORT || 3000;
     
 // })()
 
-app.listen(port, "0.0.0.0", function () {
+app.listen(port, "0.0.0.0", () => {
   console.log(`App listening on port: ${port}`);
 });
 
