f976669e37a50e0ebbc4a518489a5caf5b4d5aea
casscadence <casscadence@gmail.com>
2024-03-02 04:53:37 -0500

    test

--- server/server.js
+++ server/server.js
@@ -25,51 +25,51 @@ const port = process.env.PORT || 3000;
   })
 }) */
 
-(async () => {
-/*     const pool = createPool({
-      host: process.env.DB_HOST,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_DATABASE,
-      connectionLimit: 10
-    })
+// (async () => {
+// /*     const pool = createPool({
+//       host: process.env.DB_HOST,
+//       user: process.env.DB_USER,
+//       password: process.env.DB_PASSWORD,
+//       database: process.env.DB_DATABASE,
+//       connectionLimit: 10
+//     })
   
-    const projectData = await new Promise((resolve) => {
-      pool.query("select * from project", (err, res) => {
-        resolve(res)
-      })
-    }) */
+//     const projectData = await new Promise((resolve) => {
+//       pool.query("select * from project", (err, res) => {
+//         resolve(res)
+//       })
+//     }) */
 
-    app.get('/testing', (req, res) => {
-      res.json({ 
-        id: 'k',
-        title: 'k',
-        date: 'k',
-        desc: 'k',
-        type: 'k',
-        tools: 'k',
-        contribution: 'k',
-        number: 'k',
-        length: 'k',
-      })
-/*       for(let i=0; i<=projectData.length-1; i++) {
-        if(projectData[i].project_title === req.params.projectId){
-          res.json({ 
-            id: projectData[i].project_id,
-            title: projectData[i].project_title,
-            date: projectData[i].project_date,
-            desc: projectData[i].project_desc,
-            type: projectData[i].project_type,
-            tools: projectData[i].project_tools,
-            contribution: projectData[i].project_contribution,
-            number: i,
-            length: projectData.length
-          })
-        }
-      } */
-    })
+//     app.get('/testing', (req, res) => {
+//       res.json({ 
+//         id: 'k',
+//         title: 'k',
+//         date: 'k',
+//         desc: 'k',
+//         type: 'k',
+//         tools: 'k',
+//         contribution: 'k',
+//         number: 'k',
+//         length: 'k',
+//       })
+// /*       for(let i=0; i<=projectData.length-1; i++) {
+//         if(projectData[i].project_title === req.params.projectId){
+//           res.json({ 
+//             id: projectData[i].project_id,
+//             title: projectData[i].project_title,
+//             date: projectData[i].project_date,
+//             desc: projectData[i].project_desc,
+//             type: projectData[i].project_type,
+//             tools: projectData[i].project_tools,
+//             contribution: projectData[i].project_contribution,
+//             number: i,
+//             length: projectData.length
+//           })
+//         }
+//       } */
+//     })
     
-})()
+// })()
 
 app.listen(port, "0.0.0.0", function () {
   console.log(`App listening on port: ${port}`);
